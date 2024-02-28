473475a7af7cad3b40a2e7614db7a571ba7252bb
daniel <123434131+deftiwnl@users.noreply.github.com>
2024-02-28 21:48:47 +0800

    Add files via upload

---
+++ ErrorHandling.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract ErrorHandlingExaxmple {
+    mapping(address => uint256) public valueMapping;
+
+    function setValue(uint256 _value) public {
+        require(_value != 0, "Value cannot be zero");
+        valueMapping[msg.sender] = _value;
+    }
+
+
+    function getValue() public view returns (uint256) {
+        require(valueMapping[msg.sender] != 0, "No value set for sender");
+        return valueMapping[msg.sender];
+    }
+}
